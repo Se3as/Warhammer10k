1bfe066eba3b62f1187a0f73c09070f9b2fc08a9
AlexSM15 <alexanderwangwu@gmail.com>
2025-06-04 19:32:52 -0600

    Implement the Exhaustive search acot algorithm

--- model/ExhaustiveSearchAcot.cpp
+++ model/ExhaustiveSearchAcot.cpp
@@ -1,4 +1,36 @@
-#include "ExhaustiveSearch.h"
+
+#include "ExhaustiveSearchAcot.h"
+#include <vector>
+#include <limits>
+#define COST_LIMIT 20 // It could change
+using namespace std;
 
 size_t exaSearchAcot(const vector<vector<Edge>>& adj,
-    vector<bool>& visited, size_t origin, size_t destination){}
+                     vector<bool>& visited, size_t origin, size_t destination) {
+    size_t best_cost = numeric_limits<size_t>::max();
+    dfs(adj, visited, origin, destination, 0, best_cost);
+    return best_cost;
+}
+
+
+size_t dfs(const vector<vector<Edge>>& adj, vector<bool>& visited,
+           size_t current, size_t destination, size_t cost, size_t& best_cost) {
+
+    if (cost > COST_LIMIT || cost >= best_cost)
+        return best_cost;
+
+    if (current == destination)
+        return best_cost = min(best_cost, cost);
+
+    visited[current] = true;
+
+    for (const Edge& edge : adj[current]) {
+        if (!visited[edge.id]) {
+            dfs(adj, visited, edge.id, destination, cost + edge.dist, best_cost);
+        }
+    }
+
+    visited[current] = false;  // Backtrack
+    return best_cost;
+}
+
