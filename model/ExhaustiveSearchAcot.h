a35d8c38e11273a77fa7fa7c54122ea0ee7aa8f0
AlexSM15 <alexanderwangwu@gmail.com>
2025-06-04 19:37:33 -0600

    Add dfs function in header

--- model/ExhaustiveSearchAcot.h
+++ model/ExhaustiveSearchAcot.h
@@ -5,3 +5,6 @@ using namespace std;
 
 size_t exaSearchAcot(const vector<vector<Edge>>& adj,
     vector<bool>& visited, size_t origin, size_t destination);
+
+size_t dfs(const vector<vector<Edge>>& adj, vector<bool>& visited,
+           size_t current, size_t destination, size_t cost, size_t& best_cost);
