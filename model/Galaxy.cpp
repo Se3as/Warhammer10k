074add1354133b6bfcc4e0f842720bf24758fd8b
AlexSM15 <alexanderwangwu@gmail.com>
2025-06-06 22:56:14 -0600

    Copied from the fix solution

--- model/Galaxy.cpp
+++ model/Galaxy.cpp
@@ -24,25 +24,26 @@ void Galaxy::makeConnections() {
     size_t n = planetarium.size();
     this->graph = Graph(n);
 
-    vector<bool> connected(n, false);
-    connected[0] = true;
-    
-    for (size_t i = 1; i < n; i++) {
-        size_t j = rand() % i; 
+    for (size_t i = 1; i < n; ++i) {
+        size_t j = rand() % i;
         size_t origin = planetarium[j]->getSectorID();
         size_t destination = planetarium[i]->getSectorID();
-        this->graph.addCon(origin, destination, 1);
-        connected[i] = true;
+
+        size_t dist = distanceTwoPoints( planetarium[j]->getPosX(), planetarium[j]->getPosY(),
+            planetarium[i]->getPosX(), planetarium[i]->getPosY());
+        this->graph.addCon(origin, destination, dist);
     }
 
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = i + 1; j < n; j++) {
-            if (this->graph.hasEdge(i, j)) continue;
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+            size_t origin = planetarium[i]->getSectorID();
+            size_t destination = planetarium[j]->getSectorID();
+            if (this->graph.hasEdge(origin, destination)) continue;
 
-            if (rand() % 3 < 1) {  
-                size_t origin = planetarium[i]->getSectorID();
-                size_t destination = planetarium[j]->getSectorID();
-                this->graph.addCon(origin, destination, 1);
+            if (rand() % 3 < 1) {
+                size_t dist = distanceTwoPoints( planetarium[j]->getPosX(), planetarium[j]->getPosY(),
+                    planetarium[i]->getPosX(), planetarium[i]->getPosY());
+                this->graph.addCon(origin, destination, dist);
             }
         }
     }
